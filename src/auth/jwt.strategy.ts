f363d77a9c90326ea133ac4f5c7ffe98d4ba6649
Priyanka Raghuvanshi <priyanka.raghuvanshi@bajajfinserv.in>
2024-07-03 15:04:38 +0530

    Ignore Expiratin

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: 'nitin', // Make sure this matches the secret used to sign the token
     });
   }
